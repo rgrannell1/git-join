23968429149d02ec419cfe1f58f1cc827a835714
rgrannell1 <r.grannell2@gmail.com>
2014-09-02 23:58:57 +0100

    began work on actual command

--- git_join.py
+++ git_join.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
+import sys
+
+
+
+
+
+
+
+
+def branch_exists (branchname):
+	return True
+
+
+
+
+
 def main (args):
 
 	branchname = args['<branchname>']
 
-
-
+	if not branch_exists(branchname):
+		raise Exception("error: pathspec '" + branchname + "' did not match any branch name.")
+		sys.exit(1)
 
