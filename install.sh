a9e0531ef5c8cda644fedb623631833c4045ec91
rgrannell1 <r.grannell2@gmail.com>
2014-09-17 00:34:31 +0100

    updated installer

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 git clone https://github.com/rgrannell1/git-join
-cd git-join
-echo alias git-join=\'$(pwd -P)/lib/git-join.sh\' >> ~/.bashrc && . ~/.bashrc
+cd git-join && cp git-join /usr/bin/
